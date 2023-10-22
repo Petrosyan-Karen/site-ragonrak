b6c16256a7c5d7c6f02a142e8546a46304aa1048
K555218 <kpn16@mail.ru>
2023-10-22 13:14:56 +0400

    first commit to upload to github

---
+++ src/assets/js/languages.js
@@ -0,0 +1,20 @@
+export const languages = {
+  en: {
+    editions: "Editions",
+    controller: "Controller",
+    about: "About",
+    explore: "Explore",
+    news: "News",
+    faq: "Faq",
+    god: "God of War Ragnarök",
+  },
+  ru: {
+    editions: "Издания",
+    controller: "Джостик",
+    about: "Об игре",
+    explore: "Исследовать",
+    news: "Новости",
+    faq: "Помашь",
+    god: "Бог войны Рагнарёк",
+  },
+};
